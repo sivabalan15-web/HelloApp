27387e6e01b829e84fb198312a8e7239f710fc66
sivabalan15-web <sivaayyapan63@gmail.com>
2026-03-26 16:23:52 +0530

    added-UC1

---
+++ HelloApp.java
@@ -0,0 +1,10 @@
+public class HelloApp {
+
+    // Main method - entry point of the program
+    public static void main(String[] args) {
+
+        // Printing Hello World to the console
+        System.out.println("Hello World");
+
+    }
+}
