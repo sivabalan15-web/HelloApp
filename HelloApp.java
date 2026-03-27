06d520815eaf1e2233b1c5629431915dc80232b9
sivabalan15-web <sivaayyapan63@gmail.com>
2026-03-27 23:11:17 +0530

    added-UC5

--- HelloApp.java
+++ HelloApp.java
@@ -1,21 +1,14 @@
-public class HelloWorld {
+public class HelloApp{
     public static void main(String[] args) {
 
+        // Check if no arguments are passed
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            System.out.print("Hello, ");
-            
-            for (int i = 0; i < args.length; i++) {
-                System.out.print(args[i]);
-
-                // Add comma between names except last
-                if (i < args.length - 1) {
-                    System.out.print(", ");
-                }
+            // Enhanced for loop (for-each)
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
             }
-
-            System.out.println("!");
         }
     }
 }
